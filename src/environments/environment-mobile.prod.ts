4750f0a36a425bb07922197172ae0bb4eeb02708
Yefferson Barrial <itaqdeveloper03@gmail.com>
2026-03-25 22:43:43 -0500

    Commit: agregar environments

---
+++ src/environments/environment-mobile.prod.ts
@@ -0,0 +1,13 @@
+// export const environment = {
+//   production: true,
+//   apiUrlAuth: 'https://marcaciongps.aquariusconsultores.com:8443/regina-billing-dev',
+//   apiUrlProcess: 'https://marcaciongps.aquariusconsultores.com:8443/regina-process-dev/api/',
+//   apiUrlUtils: "http://38.187.16.148:21678/sai-web-utils-dev/api/utils/",
+//   apiUrlMaestros: 'https://marcaciongps.aquariusconsultores.com:8443/regina-process-dev/api/',
+//   apiUrlIA: 'http://localhost:6700',
+//   apiUrlOcr: 'http://localhost:6701'
+// };
+
+export const environment = {
+  production: true
+};
---
+++ src/environments/environment-mobile.ts
@@ -0,0 +1,26 @@
+// export const environment = {
+//   production: false,
+//   apiUrlAuth: 'https://marcaciongps.aquariusconsultores.com:8443/regina-billing-dev',
+//   apiUrlProcess: 'https://marcaciongps.aquariusconsultores.com:8443/regina-process-dev/api/',
+//   apiUrlUtils: "http://38.187.16.148:21678/sai-web-utils-dev/api/utils/",
+//   apiUrlMaestros: 'https://marcaciongps.aquariusconsultores.com:8443/regina-process-dev/api/',
+//   apiUrlIA: 'http://localhost:6700',
+//   apiUrlOcr: 'http://localhost:6701'
+// };
+
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
